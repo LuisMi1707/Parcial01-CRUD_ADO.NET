3603bce45a4b50aaf5321407fd76ed87cfa41594
LuisMi1707 <TuCorreo@example.com>
2024-08-30 18:55:30 -0600

    recomentando CapaNegocios (capaLogica)

--- CRUD-ADO.NET/CapaNegocio/CN_Productos.cs
+++ CRUD-ADO.NET/CapaNegocio/CN_Productos.cs
@@ -11,8 +11,10 @@ namespace CapaNegocio
 {
     public class CN_Productos
     {
+        // Instancia de la capa de datos.
         private CD_Productos objetoCD = new CD_Productos();
 
+        // Método para mostrar los productos.
         public DataTable MostrarProd()
         {
 
@@ -20,23 +22,28 @@ namespace CapaNegocio
             tabla = objetoCD.Mostrar();
             return tabla;
         }
+
+        // Método para insertar un producto
         public void InsertarPRod(string nombre, string desc, string marca, string precio, string stock)
         {
 
             objetoCD.Insertar(nombre, desc, marca, Convert.ToDouble(precio), Convert.ToInt32(stock));
         }
 
+        // Método para editar un producto
         public void EditarProd(string nombre, string desc, string marca, string precio, string stock, string id)
         {
             objetoCD.Editar(nombre, desc, marca, Convert.ToDouble(precio), Convert.ToInt32(stock), Convert.ToInt32(id));
         }
 
+        // Método para eliminar un producto
         public void EliminarPRod(string id)
         {
 
             objetoCD.Eliminar(Convert.ToInt32(id));
         }
 
+        // Método para buscar un producto por nombre
         public DataTable BuscarProducto(string nombre)
         {
             return objetoCD.Buscar(nombre);
